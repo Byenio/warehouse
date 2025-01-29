9e89ed4e7ecee3a4862326062c2f8a64740f4970
byenio <marcel.bienko@gmail.com>
2025-01-29 17:39:02 +0100

    feat: product deletion request

--- client/app/actions.ts
+++ client/app/actions.ts
@@ -3,17 +3,14 @@
 import { env } from "process";
 
 export async function deleteProductAction(productId: string) {
-  console.log(
-    `http://${env.SERVER_ADDRESS}:${env.SERVER_PORT}/api/products/${productId}`
-  );
-  // const response = await fetch(`http://${env.SERVER_ADDRESS}:${env.SERVER_PORT}/api/products/${productId}`, {
-  //     method: "DELETE",
-  //     headers: {
-  //         "Content-Type": "application/json",
-  //     },
-  // });
+    const response = await fetch(
+        `http://${env.SERVER_ADDRESS}:${env.SERVER_PORT}/api/product/${productId}`,
+        {
+            method: "DELETE",
+        }
+    );
 
-  // if (!response.ok) {
-  //     throw new Error("Failed to delete product");
-  // }
+    if (response.status != 204) {
+        throw new Error("Failed to delete product");
+    }
 }
